515c2222d779dfca4370cc37737bfcf80c180762
DarshanaUOP <46531949+DarshanaUOP@users.noreply.github.com>
2020-04-13 01:23:03 +0530

    list view hardcoded for organizations

--- app/src/main/java/com/example/darshana/moneymanager/Organization.java
+++ app/src/main/java/com/example/darshana/moneymanager/Organization.java
@@ -3,16 +3,24 @@ package com.example.darshana.moneymanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adapters.AdapterOrganization;
+import entities.EntityOrganization;
+
 public class Organization extends AppCompatActivity {
 
     FloatingActionButton btnAddNewOrg;
-
+    ListView listViewOrg;
+    List<EntityOrganization> orgList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,20 @@ public class Organization extends AppCompatActivity {
                 }
             }
         });
+
+        listViewOrg = (ListView) findViewById(R.id.listViewOrg);
+        orgList = new ArrayList<EntityOrganization>();
+
+        try {
+            Organization.this.orgList.add(new EntityOrganization(R.mipmap.ic_launcher_round,"gotoZero","ANGO"));
+            Organization.this.orgList.add(new EntityOrganization(R.mipmap.ic_launcher_round,"gotoZero","ANGO"));
+            Organization.this.orgList.add(new EntityOrganization(R.mipmap.ic_launcher_round,"gotoZero","ANGO"));
+        }catch (Exception e){
+            showMassages("Error on loading : " + e.getMessage());
+        }
+
+        Organization.this.listViewOrg.setAdapter(new AdapterOrganization(Organization.this,this.orgList));
+
 
     }
 
